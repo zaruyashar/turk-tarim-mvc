e596e6f2f9d044d0382da02826b80c53f09b34c8
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-03-04 16:18:07 +0300

    Team/Abbouncement/Gallery sections addition

---
+++ AgriculturePresentation/ViewComponents/_GalleryImagePartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _GalleryImagePartial : ViewComponent
+    {
+        private readonly IGalleryImageService _galleryImageService;
+
+        public _GalleryImagePartial(IGalleryImageService galleryService)
+        {
+            _galleryImageService = galleryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _galleryImageService.GetListAll();
+            return View(values);
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20260304114523_mig_class-additions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_classadditions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class1",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Class2",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class1",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "Class2",
+                table: "Services");
+        }
+    }
+}
