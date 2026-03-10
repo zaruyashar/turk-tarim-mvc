ce40cafe5a42493de10d9a4e2c3ec5330f261d7e
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-03-10 03:58:29 +0300

    feat(ui): complete dashboard overhaul, fix layout syntax, and implement dynamic components

---
+++ AgriculturePresentation/ViewComponents/_NavbarMessageComponent.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _NavbarMessageComponent : ViewComponent
+    {
+        private readonly IContactService _contactService;
+
+        public _NavbarMessageComponent(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _contactService.GetListAll().OrderByDescending(x => x.Date).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
