16b33b6ec6310d033952a19749dd716aa6fb0da8
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-03-13 13:59:18 +0300

    fix: img validator char limit updated

--- BusinessLayer/ValidationRules/GalleryImageValidator.cs
+++ BusinessLayer/ValidationRules/GalleryImageValidator.cs
@@ -13,10 +13,10 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.Title).NotEmpty().WithMessage("Görsel başlığı boş bırakılamaz.");
             RuleFor(x => x.Description).NotEmpty().WithMessage("Görsel açıklaması boş bırakılamaz.");
             RuleFor(x => x.GalleryImageUrl).NotEmpty().WithMessage("Görsel yolu boş bırakılamaz.");
-            RuleFor(x => x.Title).MaximumLength(20).WithMessage("Başlık en fazla 20 karakterden oluşmalıdır.");
-            RuleFor(x => x.Title).MinimumLength(8).WithMessage("Başlık en az 20 karakterden oluşmalıdır.");
-            RuleFor(x => x.Description).MaximumLength(50).WithMessage("Açıklama en fazla 50 karakterden oluşmalıdır.");
-            RuleFor(x => x.Description).MinimumLength(20).WithMessage("Açıklama en az 20 karakterden oluşmalıdır.");
+            RuleFor(x => x.Title).MaximumLength(30).WithMessage("Başlık en fazla 30 karakterden oluşmalıdır.");
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("Başlık en az 5 karakterden oluşmalıdır.");
+            RuleFor(x => x.Description).MaximumLength(100).WithMessage("Açıklama en fazla 100 karakterden oluşmalıdır.");
+            RuleFor(x => x.Description).MinimumLength(10).WithMessage("Açıklama en az 10 karakterden oluşmalıdır.");
         }
     }
 }
